50fdda339acc0966952ff47e5a027b23e1878e01
rezaabdullah <air.reza@hotmail.com>
2022-06-24 22:58:19 +0600

    feat: removed setup.cfg and pyproject.toml

--- setup.py
+++ setup.py
@@ -8,28 +8,62 @@
 
 from setuptools import setup
 
-if __name__ == '__main__':
-    setup()
+# if __name__ == '__main__':
+#     setup()
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
 
 # setup(
 #     name="dashboard-etl"
 #     version="1.0.0",
 #     author="Abdullah Reza",
 #     description="ETL project for Google Data Studio Dashboard",
-#     long_description='This is a longer description for the project',
-#     url='https://medium.com/@gmyrianthous',
-#     keywords='sample, example, setuptools',
-#     python_requires='>=3.7, <4',
-#     install_requires=['pandas'],
+#     long_description=long_description,
+#     long_description_content_type="text/markdown"
+#     url="https://github.com/rezaabdullah/dashboard-etl",
+#     keywords=["etl, data visualization, analytics, dashboard"],
+#     license = "Apache License 2.0",
+#     classifiers=[
+#         "License :: OSI Approved :: Apache Software License",
+#         "Development Status :: 3 - Alpha",
+#         "Intended Audience :: Developers",
+#         "Programming Language :: Python :: 3",
+#         "Programming Language :: Python :: 3.7",
+#         "Programming Language :: Python :: 3.8"
+#     ],
+#     python_requires=">=3.8",
+#     install_requires=["pandas >= 1.3.0",
+#         "SQLAlchemy >= 1.4.30",
+#         "python-dotenv >= 0.19.0"],
 #     extras_require={
-#         'test': ['pytest', 'coverage'],
+#         "notebook": ["jupyter >= 1.0.0", "seaborn"],
+#         "dev": ["flake8 >= 4.0.1", "black >= 22.3.0"]
 #     },
-#     package_data={
-#         'sample': ['example_data.csv'],
-#     },
+#     # package_data={
+#     #     'sample': ['example_data.csv'],
+#     # },
 #     entry_points={
-#         'runners': [
-#             'sample=sample:main',
+#         "console_scripts": [
+#             "etl = etl.__main__:main"
 #         ]
 #     }
 # )
+
+setup(
+    # name='mypackage',
+    # version='0.0.1',
+    # install_requires=[
+    #     'requests',
+    #     'importlib-metadata; python_version == "3.8"',
+    # ],
+    name="dashboard-etl",
+    version="1.0.0",
+    author="Abdullah Reza",
+    description="ETL project for Google Data Studio Dashboard",
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+    # url="https://github.com/rezaabdullah/dashboard-etl",
+#     keywords=["etl, data visualization, analytics, dashboard"],
+#     license = "Apache License 2.0",
+)
