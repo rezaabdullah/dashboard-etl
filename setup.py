5f70c29b8d41c5604fd1b00e1ee1631ffb7b6498
rezaabdullah <air.reza@hotmail.com>
2022-06-24 22:32:52 +0600

    feat: remove setup_requires in setup.cfg

--- setup.py
+++ setup.py
@@ -1,7 +1,35 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-setup(
-    name='dashboard-etl',
-    version='1.0.0',
-    packages=find_packages(include=['exampleproject', 'exampleproject.*'])
-)
+# setup(
+#     name='dashboard-etl',
+#     version='1.0.0',
+#     packages=find_packages(include=['exampleproject', 'exampleproject.*'])
+# )
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup()
+
+# setup(
+#     name="dashboard-etl"
+#     version="1.0.0",
+#     author="Abdullah Reza",
+#     description="ETL project for Google Data Studio Dashboard",
+#     long_description='This is a longer description for the project',
+#     url='https://medium.com/@gmyrianthous',
+#     keywords='sample, example, setuptools',
+#     python_requires='>=3.7, <4',
+#     install_requires=['pandas'],
+#     extras_require={
+#         'test': ['pytest', 'coverage'],
+#     },
+#     package_data={
+#         'sample': ['example_data.csv'],
+#     },
+#     entry_points={
+#         'runners': [
+#             'sample=sample:main',
+#         ]
+#     }
+# )
