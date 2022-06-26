b282de33e7fb0f364fc08bffad0f0f4bc71d0106
rezaabdullah <air.reza@hotmail.com>
2022-06-27 02:41:44 +0600

    feat: Transformer function for income

--- etl/master_transform/transformer.py
+++ etl/master_transform/transformer.py
@@ -3,8 +3,12 @@ import pandas as pd
 # normalize dataset for granular analysis
 def transformer(df):
     # filter income generating dataset i.e. sale, machine rent and advisory
-    cash_in = df.loc[df["transaction_category"].isin(["Sale", "Advisory",
+    income = df.loc[df["transaction_category"].isin(["Sale", "Advisory",
         "Machinery Rental"])]
-    
-    cash_in.to_csv("cash_in.csv", index=False)
-    return df
+
+    # drop unnecessary columns
+    income.drop(columns=["supplier_id", "supplier_name", "supplier_mobile", "supplier_gender",
+        "product_expenditure", "expenditure", "product_expenditure_usd", "expenditure_usd",
+        "expense_category", "direct_cost_usd", "indirect_cost_usd"], inplace=True)
+
+    income.to_csv("income.csv", index=False)
