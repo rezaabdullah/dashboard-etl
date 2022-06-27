7bb582e28825ffc678b5c6276b31101a541be0ed
rezaabdullah <air.reza@hotmail.com>
2022-06-27 21:18:43 +0600

    feat: Renamed customer columns

--- etl/master_transform/transformer.py
+++ etl/master_transform/transformer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sqlalchemy import column
 
 # normalize dataset for granular analysis
 def transformer(df):
@@ -10,5 +11,8 @@ def transformer(df):
     income.drop(columns=["supplier_id", "supplier_name", "supplier_mobile", "supplier_gender",
         "product_expenditure", "expenditure", "product_expenditure_usd", "expenditure_usd",
         "expense_category", "direct_cost_usd", "indirect_cost_usd"], inplace=True)
+    
+    # rename customer information with person prefix
+    income.rename(columns=["person_id", "person_name", "person_mobile", "person_gender"], inplace=True)
 
     income.to_csv("income.csv", index=False)
